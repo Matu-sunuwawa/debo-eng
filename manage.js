74eeb4d7283acd2aa995c2ae9e30ebf8b79b3be0
Matyas Sina <matyassinaadugna@gmail.com>
2023-08-10 08:53:47 -0700

    first commit

---
+++ manage.js
@@ -0,0 +1,305 @@
+
+
+function seemore()
+{
+    var abc =document.getElementById("id11");
+    abc.innerHTML=`<p>I'm young Multidisciplinary , Innovator, Entrepreneur, Engineer, Leader, Lecturer, Researcher , Change Maker,
+    Moreover,
+    I'm long life LEARNER.
+    
+    I have studied BSc in Computer Science, Civil Engineering , Msc in Computer Networking and MA in Project Management and Finance.
+    Now I'm serving as lecturer at Jimma University and CEO at Debo Engineering Corporate.
+    I'm the award winner of Green Innovation and Agritech Slam 2019, MEST Africa challenge 2020 and coffee innovation fund 2021 and Won 
+    Excelling Innovation award of HEART 2021 Convention and Innovation Excellence 2022. Following my extraordinary contribution and achievement in science and technology,
+    innovation and business I have featured and presented on more than 30 local and international medias. I have shared my success story and as result many young peoples are motivated and encouraged.</p>`;
+    abc.style.transition="1s";
+}
+function seeall()
+{
+    var abc2= document.getElementById("id22");
+    var abc3= document.getElementById("id222");
+    var disappear= document.getElementById("butt2");
+    abc2.style.display="flex";
+    abc3.style.display="flex";
+    disappear.style.display="none";
+
+}
+function seemore2()
+{
+    var abc4 =document.getElementById("id33");
+    abc4.innerHTML=`                    
+    <h3>Debo Engineering Corporate</h3>
+    <p style="color: rgba(0, 0, 0, 0.6);">4 years 8 months</p><br>
+
+    <h3>Chief Executive Officer</h3>
+    <h4>Oct 2018 - Present4 years 8 months</h4>
+    <p style="color: rgba(0, 0, 0, 0.6);">Jimma, Oromia Region, Ethiopia</p><br>
+
+    <h3>Network, Civil and Softare Engineer</h3>
+    <h4>Oct 2018 - Present4 years 8 months</h4>
+    <p style="color: rgba(0, 0, 0, 0.6);">Jimma</p><br>
+    <p style="color: rgba(0, 0, 0, 0.6);">Jermia Bayisa is an energetic and creative young Engineer, Innovator, Entrepreneur, Programmer, 
+    multidisciplinary and Lecturer at Jimma University and they got national and international recognition 
+    and awards for their innovative service.</p><br><hr><br>`;
+    abc4.style.transition="1s";
+
+}
+function seemore3()
+{
+    var abc5= document.getElementById("id44");
+    abc5.innerHTML=`Computer science, Civil Engineering, Computer Networking and project mgt and FinanceBsc in Computer Science and Civil Eng, 
+    Msc in Computer Networking, MA in project mgt and FinanceMasters
+    2012 - 2019
+    
+    Activities and Societies: Co-founder and CEO of Debo Engineering, Network Technician, ICT consultant
+    
+    I'm Young Energetic and highly motivated Entrepreneur, Innovator, lecturer, 
+    AI Researcher and multi disciplinary with tone of experiences.`;
+    abc5.style.color="rgba(0, 0, 0, 0.6);";
+}
+function seemore4()
+{
+    var abc6= document.getElementById("id6666");
+    abc6.innerHTML=`“I know Mr. Jermia for over 3 years where we both teach. 
+    I found that Mr. Jermia is a brilliant young man. 
+    As a former Dean of the Faculty of Computing and Informatics, 
+    Jimma University, I gave him several assignments. 
+    He delivers more than what is expected from him. 
+    Jeremia is a very hardworking, team player and meets deadlines. 
+    He is someone you can count on. Thus, I recommend Mr. 
+    Jermia to any position in his area of interest.”`;
+}
+function seemore5()
+{
+    var abc7= document.getElementById("id66");
+    var disappear= document.getElementById("id666");
+    abc7.style.display="block";
+    disappear.style.display="none";
+}
+
+function change1()
+{
+    // $(document).ready(function(){
+    //     $("button").hover(function(){
+    //         $(this).css("background-color","black");},
+    //          function(){
+    //             $(this).css("background-color","pink");
+    //          });
+    // });
+    var change1=document.getElementById("id0");
+    var change2=document.getElementById("id1");
+    var change3=document.getElementById("id2");
+    var change4=document.getElementById("id3");
+    var change5=document.getElementById("id4");
+    var change6=document.getElementById("id5");
+    var change7=document.getElementById("id6");
+    var change8=document.getElementById("id7");
+    var change9=document.getElementById("id8");
+    var change10=document.getElementById("id9");
+    var butt1=document.getElementById("butt2");
+    var butt2=document.getElementById("id666");
+    var skill=document.getElementsByClassName("skill22");
+    var form=document.getElementById("form1");
+    var inputs=document.getElementsByClassName("input-field");
+    var butt3=document.getElementById("submit-btn");
+    change1.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    change1.style.transition="1s";
+    change2.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    change2.style.transition="2s";
+    change3.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    change3.style.transition="3s";
+    change4.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    change4.style.transition="4s";
+    change5.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    change5.style.transition="5s";
+    change6.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    change6.style.transition="1s";
+    change7.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    change7.style.transition="1s";
+    change8.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    change8.style.transition="1s";
+    change9.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    change9.style.transition="1s";
+    change10.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    change10.style.transition="1s";
+    butt1.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    butt1.style.transition="1s";
+    butt2.style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    butt2.style.transition="1s";
+    butt3.style.backgroundColor="rgba(255, 165, 0, 1)";
+    butt3.style.transition="1s";
+    form.style.backgroundColor="rgba(255, 165, 0, 0.7)";
+    form.style.transition="1s";
+    inputs[0].style.backgroundColor="rgba(255, 165, 0, 1)";
+    inputs[1].style.backgroundColor="rgba(255, 165, 0, 1)";
+    inputs[2].style.backgroundColor="rgba(255, 165, 0, 1)";
+    inputs[3].style.backgroundColor="rgba(255, 165, 0, 1)";
+    inputs[4].style.backgroundColor="rgba(255, 165, 0, 1)";
+
+    skill[0].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[1].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[2].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[3].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[4].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[5].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[6].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[7].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[8].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill[9].style.backgroundColor="rgba(255, 165, 0, 0.5)";
+    skill.style.transition="1s";
+}
+function change2()
+{
+    var change1=document.getElementById("id0");
+    var change2=document.getElementById("id1");
+    var change3=document.getElementById("id2");
+    var change4=document.getElementById("id3");
+    var change5=document.getElementById("id4");
+    var change6=document.getElementById("id5");
+    var change7=document.getElementById("id6");
+    var change8=document.getElementById("id7");
+    var change9=document.getElementById("id8");
+    var change10=document.getElementById("id9");
+    var butt1=document.getElementById("butt2");
+    var butt2=document.getElementById("id666");
+    var skill=document.getElementsByClassName("skill22");
+    var form=document.getElementById("form1");
+    var inputs=document.getElementsByClassName("input-field");
+    var butt3=document.getElementById("submit-btn");
+    change1.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    change1.style.transition="1s";
+    change2.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    change2.style.transition="2s";
+    change3.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    change3.style.transition="3s";
+    change4.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    change4.style.transition="4s";
+    change5.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    change5.style.transition="5s";
+    change6.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    change6.style.transition="1s";
+    change7.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    change7.style.transition="1s";
+    change8.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    change8.style.transition="1s";
+    change9.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    change9.style.transition="1s";
+    change10.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    change10.style.transition="1s";
+    butt1.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    butt1.style.transition="1s";
+    butt2.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    butt2.style.transition="1s";
+    butt3.style.backgroundColor="rgba(0, 255, 255, 1)";
+    butt3.style.transition="1s";
+    form.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    form.style.transition="1s";
+    inputs[0].style.backgroundColor="rgba(0, 255, 255, 1)";
+    inputs[1].style.backgroundColor="rgba(0, 255, 255, 1)";
+    inputs[2].style.backgroundColor="rgba(0, 255, 255, 1)";
+    inputs[3].style.backgroundColor="rgba(0, 255, 255, 1)";
+    inputs[4].style.backgroundColor="rgba(0, 255, 255, 1)";
+
+    skill[0].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[1].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[2].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[3].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[4].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[5].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[6].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[7].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[8].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill[9].style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    skill.style.transition="1s";
+
+
+    // change9.style.backgroundColor="rgba(0, 255, 255, 0.5)";
+    // change9.style.transition="1s";
+    // change10.style.backgroundColor="rgba(0, 255, 255, 0.7)";
+    // change10.style.transition="1s";
+}
+function change3()
+{
+    var change1=document.getElementById("id0");
+    var change2=document.getElementById("id1");
+    var change3=document.getElementById("id2");
+    var change4=document.getElementById("id3");
+    var change5=document.getElementById("id4");
+    var change6=document.getElementById("id5");
+    var change7=document.getElementById("id6");
+    var change8=document.getElementById("id7");
+    var change9=document.getElementById("id8");
+    var change10=document.getElementById("id9");
+    var butt1=document.getElementById("butt2");
+    var butt2=document.getElementById("id666");
+    var skill=document.getElementsByClassName("skill22");
+    var form=document.getElementById("form1");
+    var inputs=document.getElementsByClassName("input-field");
+    var butt3=document.getElementById("submit-btn");
+    change1.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    change1.style.transition="1s";
+    change2.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    change2.style.transition="2s";
+    change3.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    change3.style.transition="3s";
+    change4.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    change4.style.transition="4s";
+    change5.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    change5.style.transition="5s";
+    change6.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    change6.style.transition="1s";
+    change7.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    change7.style.transition="1s";
+    change8.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    change8.style.transition="1s";
+    change9.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    change9.style.transition="1s";
+    change10.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    change10.style.transition="1s";
+    butt1.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    butt1.style.transition="1s";
+    butt2.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    butt2.style.transition="1s";
+    butt3.style.backgroundColor="rgba(127, 255, 212, 1)";
+    butt3.style.transition="1s";
+    form.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    form.style.transition="1s";
+    inputs[0].style.backgroundColor="rgba(127, 255, 212, 1)";
+    inputs[1].style.backgroundColor="rgba(127, 255, 212, 1)";
+    inputs[2].style.backgroundColor="rgba(127, 255, 212, 1)";
+    inputs[3].style.backgroundColor="rgba(127, 255, 212, 1)";
+    inputs[4].style.backgroundColor="rgba(127, 255, 212, 1)";
+
+    skill[0].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[1].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[2].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[3].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[4].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[5].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[6].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[7].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[8].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill[9].style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    skill.style.transition="1s";
+
+
+    // change9.style.backgroundColor="rgba(127, 255, 212, 0.5)";
+    // change9.style.transition="1s";
+    // change10.style.backgroundColor="rgba(127, 255, 212, 0.7)";
+    // change10.style.transition="1s";
+}
+function doit()
+{
+    var id1=document.getElementById("id-1").value;
+    var id2=document.getElementById("id-2").value;
+    var id3=document.getElementById("id-3").value;
+    var id7=document.getElementById("id-7").value;
+
+    if(id1!='' && id2!='' && id3!='' && id7!='')
+    {
+        if(typeof(id1)!=String || typeof(id1)!=String)
+        {
+            alert("Please Insert The Valid Name!");
+        }
+    }
+}
